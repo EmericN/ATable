f2d49685e07239294e1deb560650486612667159
Nicot Emeric <emeric.nicot@sfr.fr>
2017-09-30 12:10:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ATable_wip2" 
--- app/src/main/java/com/emeric/nicot/atable/RefreshCallback.java
+++
@@ -1,9 +0,0 @@
-package com.emeric.nicot.atable;
-
-/**
- * Created by Nicot Emeric on 09/08/2017.
- */
-
-interface RefreshCallback {
-    void RefreshCallback();
-}
